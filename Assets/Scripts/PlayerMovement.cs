0af643e846cc4abcc1ef12ed6a4f9b92533af32e
Kirill Prakhov <kirya_prahov@mail.ru>
2024-01-01 15:21:18 +0400

    feat: add player camera movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float sensivity = 50f;
+    private GameObject Camera;
+    Vector3 currentEulerAngles;
+    void Start()
+    {
+        Camera = GameObject.Find("Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Mouse X");
+        float verticalInput = Input.GetAxis("Mouse Y");
+        currentEulerAngles += new Vector3(verticalInput * -1, horizontalInput, 0) * Time.deltaTime * sensivity;
+        Camera.transform.eulerAngles = currentEulerAngles;
+        Debug.Log(Camera.transform.rotation);
+        
+        
+        
+    }
+}
