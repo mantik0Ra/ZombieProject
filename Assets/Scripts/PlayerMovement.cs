581cfd365ba4db009eb8be1f9569816bd089b078
Kirill Prakhov <kirya_prahov@mail.ru>
2024-01-02 14:47:48 +0400

    refactor: add function shoot and rename variable as CanShoot

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
     ParticleSystem particleSystem;
 
-    bool Shoot = false;
+    bool CanShoot = false;
     void Start()
     {
         Camera = GameObject.Find("Camera");
@@ -27,29 +27,34 @@ public class PlayerMovement : MonoBehaviour
         currentEulerAngles += new Vector3(verticalInput * -1, horizontalInput, 0) * Time.deltaTime * sensivity;
         Camera.transform.eulerAngles = currentEulerAngles;
         
-        if(Input.GetKey(KeyCode.Mouse0) && !Shoot) {
+        if(Input.GetKey(KeyCode.Mouse0) && !CanShoot) {
             particleSystem.Play();
-            Shoot = true;
-            Debug.Log(Shoot);
+            CanShoot = true;
+            Debug.Log(CanShoot);
             StartCoroutine(Coroutine());
         }
     }
 
     private void FixedUpdate() {
-        if(Shoot) {
-            RaycastHit hit;
-            Physics.Raycast(Camera.transform.position, Camera.transform.TransformDirection(Vector3.forward), out hit, 100f);
-            if(hit.collider is not null && hit.collider.tag == "Zombie") {
-                Debug.Log("Hit zombie");
-            }
+        if(CanShoot) {
+            Shoot();
         }
         Debug.DrawRay(Camera.transform.position, Camera.transform.TransformDirection(Vector3.forward) * 100f, Color.blue);
     }
 
     IEnumerator Coroutine() {
         yield return new WaitForSeconds(.4f);
-        Shoot = false;
+        CanShoot = false;
     }
+    void Shoot() {
+        RaycastHit hit;
+        Physics.Raycast(Camera.transform.position, Camera.transform.TransformDirection(Vector3.forward), out hit, 100f);
+        if (hit.collider is not null && hit.collider.tag == "Zombie") {
+            Debug.Log("Hit zombie");
+        }
+    }
+    
+    
 
 
 }
