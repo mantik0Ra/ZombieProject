46f8a94b37a663bebdef7cf5913ccd0b0e0f88d9
Kirill Prakhov <kirya_prahov@mail.ru>
2024-01-05 15:43:07 +0400

    feat: add GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private int coins = 0;
+
+    public int Coins {
+        get { return coins; }
+        set { coins = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
